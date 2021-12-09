1ecb02d71d01be2c9616f1f15e86b037e08a3db3
PRASANTH E <prasanth.e@hcl.com>
2021-12-09 23:14:24 +0530

    Graded

---
+++ src/main/java/com/boot/demo/PrasanthGradedAssignmentWeek10Application.java
@@ -0,0 +1,14 @@
+package com.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrasanthGradedAssignmentWeek10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrasanthGradedAssignmentWeek10Application.class, args);
+		System.out.println("Hello DEV-OPS");
+	}
+
+}
